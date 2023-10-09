822da034af30e31530887fa8d866b247071f956f
Zwogli <Mathiaskohler@web.de>
2023-10-09 17:01:25 +0200

    create model user

---
+++ src/models/user.class.ts
@@ -0,0 +1,23 @@
+export class User{
+
+  firstName: string;
+  lastName: string;
+  birthDate: number;
+  street: string;
+  zipCode: number;
+  city: string;
+
+  /**[?] = [obj ? obj.firstName : '';] if obj.firstName empty, use empty String.
+   * 
+   * @param obj - Json
+   */
+  constructor(obj?:any){
+    this.firstName = obj ? obj.firstName : '';
+    this.lastName = obj ? obj.lastName : '';
+    this.birthDate = obj ? obj.birthDate : '';
+    this.street = obj ? obj.street : '';
+    this.zipCode = obj ? obj.zipCode : '';
+    this.city = obj ? obj.city : '';
+  }
+
+  }
