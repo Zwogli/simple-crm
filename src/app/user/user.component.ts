e31760f6fb3044f4d5ba53217ea23dc882538a05
Zwogli <Mathiaskohler@web.de>
2023-10-12 11:55:37 +0200

    add subscribe user db & show on table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,66 @@
-import { Component } from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
+import {
+  Firestore,
+  collection,
+  doc,
+  onSnapshot,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
-  user = new User();  //user(variable) = new User (instance)
-  
+  user = new User(); //user(variable) = new User (instance)
+  allUsers = [];
+  unsubUser;
+  firestore: Firestore = inject(Firestore);
+
   constructor(public dialog: MatDialog) {}
-  
-  openDialog(){
+
+  ngOnInit() {
+    this.readSubscribeUser();
+  }
+
+  openDialog() {
     this.dialog.open(DialogAddUserComponent);
+  }
+
+  readSubscribeUser(){
+    this.unsubUser = onSnapshot(this.getUserReference(), (userList) => {
+      this.allUsers = [];
+      userList.forEach(userListElement =>{
+        console.log('User Liste: ', userListElement.data())
+        this.allUsers.push(userListElement.data());
+      });
+      console.log(this.allUsers);
+    });
+  }
+
+  /**return firestore collection
+   *
+   * @returns
+   */
+  getUserReference() {
+    return collection(this.firestore, 'user');
+  }
+
+  /**return a single field
+   *
+   * @param collId - firestor dokument id
+   * @param docId - firestore field id
+   * @returns
+   */
+  getSingleDocReference(collId: string, docId: string) {
+    return doc(collection(this.firestore, collId), docId);
+  }
+
+  /**unsubscribe database*/
+  destroySubscribe(){
+    this.unsubUser();
   }
 }
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,6 +1,7 @@
 export class User {
   firstName: string;
   lastName: string;
+  email: string;
   birthDate: number;
   street: string;
   zipCode: number;
@@ -13,6 +14,7 @@ export class User {
   constructor(obj?: any) {
     this.firstName = obj ? obj.firstName : '';
     this.lastName = obj ? obj.lastName : '';
+    this.email = obj ? obj.email : '';
     this.birthDate = obj ? obj.birthDate : '';
     this.street = obj ? obj.street : '';
     this.zipCode = obj ? obj.zipCode : '';
@@ -22,7 +24,8 @@ export class User {
   public toJSON() {
     return {
       firstName: this.firstName, 
-      lastName: this.lastName, 
+      lastName: this.lastName,
+      email: this.email,  
       birthDate: this.birthDate, 
       street: this.street, 
       zipCode: this.zipCode, 
